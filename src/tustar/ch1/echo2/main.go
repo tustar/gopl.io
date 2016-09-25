9e492e87b19ebc789eb959877b935f4f798a2288
tustar <tustar1030@126.com>
2016-09-25 21:12:27 +0800

    Init code

---
+++ src/tustar/ch1/echo2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for index, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+		fmt.Print(index)
+		fmt.Print("=>")
+		fmt.Println(arg)
+	}
+	fmt.Println(s)
+}
